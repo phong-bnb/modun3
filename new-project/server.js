4b2ae54462d7e9e38c78df00a56e948309d53a69
Phong <ngphong.0708@gmail.com>
2023-05-16 14:47:50 +0700

    first commit

--- new-project/server.js
+++ new-project/server.js
@@ -1,16 +1,22 @@
 const http = require('http')
 const fs = require('fs')
-let server = http.createServer((req,res)=>{
-    fs.readFile('./index.html','utf8',function(err,str){
-        if(err){
-            res.writeHead(404,{'Content-Type':'text/html'})
-return res.end("404 not found");
+let server = http.createServer(((req, res) => {
+
+    fs.readFile('./index.html','utf8', function (err,str){
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            console.log(err.message)
+            return res.end("404 Not Found");
         }
-        res.writeHead(200,{ 'Content-Type':'text/html'})
-        res.write(str)
+        // Thực hiện replace nội dung
+        let username = "Admin";
+        str = str.replace('{username}', username)
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(str);
         return res.end();
     });
+}));
+
+server.listen('8080','localhost', function () {
+    console.log(`Server running in http://localhost:8080`)
 });
-server.listen('8080',function (){
-    console.log('ngu')
-})
